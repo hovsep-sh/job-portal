c4f242dbc1cf8ba75531cefc2a1a0b56da0140db
hovsep-sh <hovsep.shirinyan@yahoo.com>
2020-02-08 03:29:35 +0400

    Updates

---
+++ precache-manifest.c867f91e692a0b2ac8b00488fd9fcacc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a89ee521fe2bb8c96b3576f4a7d193",
+    "url": "/job-portal/index.html"
+  },
+  {
+    "revision": "181be853e33847774100",
+    "url": "/job-portal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b36e93dd767f83cf9a10",
+    "url": "/job-portal/static/js/2.3970b87f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/job-portal/static/js/2.3970b87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181be853e33847774100",
+    "url": "/job-portal/static/js/main.dcf3aaf0.chunk.js"
+  },
+  {
+    "revision": "d89864054e1f55cdeab9",
+    "url": "/job-portal/static/js/runtime-main.fc6076d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/job-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
